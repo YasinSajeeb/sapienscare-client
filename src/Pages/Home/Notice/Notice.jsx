d6f50cd04d2d60c8f4a1805e06a9048f4112535d
YasinSajeeb <yeasinalhossen@gmail.com>
2024-08-08 21:37:17 +0600

    updated Notice

--- src/Pages/Home/Notice/Notice.jsx
+++ src/Pages/Home/Notice/Notice.jsx
@@ -5,7 +5,7 @@ const Notice = () => {
         <div className="notice-container skeleton rounded-none">
             <p className="notice-text">
                 <span className="font-bold">📢 Notice: </span>
-                As our supplements are out of stock, shipments are closed now. It will be started again from <span className="font-bold">August 8, 2024.</span> Pre-order your supplement now to get quickly.
+                As our supplements are out of stock, shipments are closed now. It will be started again from <span className="font-bold">August 13, 2024.</span> Pre-order your supplement now to get quickly.
                 <span className="text-red-700 font-bold"> Don't order same product twice!!</span>
             </p>
         </div>
